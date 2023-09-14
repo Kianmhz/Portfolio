747345ca63d500e11cc0a7eb4b61228c97c7e480
Kianmehr Haddad <kianmehr1777@gmail.com>
2023-09-14 19:38:01 -0400

    Fixed contrast of logos.

--- pages/index.vue
+++ pages/index.vue
@@ -245,16 +245,6 @@ onMounted(() => {
     </div>
 
     <div class="about">
-        <div class="linkedin-pic">
-            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
-                <path fill="#0288D1"
-                    d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z">
-                </path>
-                <path fill="#FFF"
-                    d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z">
-                </path>
-            </svg>
-        </div>
         <div class="container">
             <div>
                 <h1>linkedin</h1>
@@ -272,15 +262,6 @@ onMounted(() => {
     </div>
 
     <div class="about">
-        <div class="gmail-pic">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="52 42 88 66">
-                <path fill="#4285f4" d="M58 108h14V74L52 59v43c0 3.32 2.69 6 6 6" />
-                <path fill="#34a853" d="M120 108h14c3.32 0 6-2.69 6-6V59l-20 15" />
-                <path fill="#fbbc04" d="M120 48v26l20-15v-8c0-7.42-8.47-11.65-14.4-7.2" />
-                <path fill="#ea4335" d="M72 74V48l24 18 24-18v26L96 92" />
-                <path fill="#c5221f" d="M52 51v8l20 15V48l-5.6-4.2c-5.94-4.45-14.4-.22-14.4 7.2" />
-            </svg>
-        </div>
         <div class="container">
             <div>
                 <h1>Email</h1>
@@ -516,32 +497,6 @@ onMounted(() => {
     color: transparent;
     -webkit-background-clip: text;
     background-clip: text;
-}
-
-.linkedin-pic {
-    @apply absolute top-[-10px] left-0 w-[15%] z-[1000];
-}
-
-.linkedin-pic svg {
-    width: 100%;
-    /* this will take the width of the .gmail-pic */
-    height: auto;
-    filter: brightness(0.6);
-    -webkit-mask-image: linear-gradient(to left, black 0%, transparent 100%);
-    mask-image: linear-gradient(to left, black 0%, transparent 100%);
-}
-
-.gmail-pic {
-    @apply absolute top-0 right-0 w-[15%] z-[1000];
-}
-
-.gmail-pic svg {
-    width: 100%;
-    /* this will take the width of the .gmail-pic */
-    height: auto;
-    filter: brightness(0.6);
-    -webkit-mask-image: linear-gradient(to right, black 0%, transparent 100%);
-    mask-image: linear-gradient(to right, black 0%, transparent 100%);
 }
 
 .about {
