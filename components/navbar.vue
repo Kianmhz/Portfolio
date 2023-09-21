eac4e7266768b8f2a2cb352f07dfc9d3360c14fc
Kianmehr Haddad <kianmehr1777@gmail.com>
2023-09-20 22:47:35 -0400

    Changed home text to a logo

--- components/navbar.vue
+++ components/navbar.vue
@@ -11,7 +11,7 @@ const scrollTo = (refName) => {
 
 <template>
     <div class="nav-bar">
-        <button @click="scrollTo('home')" class="nav"></button>
+        <button @click="scrollTo('home')" class="nav"><img class="img2" src="~/assets/img/sign.png" alt="Home"></button>
         <button @click="scrollTo('whatIDo')" class="nav">What I do</button>
         <button @click="scrollTo('projects')" class="nav">Projects</button>
         <button @click="scrollTo('resume')" class="nav">Resume</button>
@@ -20,11 +20,25 @@ const scrollTo = (refName) => {
 
 <style scoped>
 .nav-bar {
-	@apply flex fixed flex-row flex-wrap justify-between w-full z-[100];
+	  @apply flex fixed flex-row flex-wrap justify-between w-full z-[100];
     background: linear-gradient(to bottom, rgb(0, 0, 0), rgba(0, 0, 0, 0));
 }
 .nav {
-	@apply flex-1 p-4 text-center text-white no-underline transition-all duration-200;
+  @apply flex-1 p-4 transition-all duration-200;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.nav img.img2 {
+  filter: invert(100%) brightness(100%);
+  height: 50px;
+  width: auto;
+  transition: all 0.5s ease-in-out;
+}
+.nav img.img2:hover {
+  content: url('~/assets/img/signGreen.png');
+  filter: brightness(100%);
 }
 .nav:hover {
     @apply text-[#00DC82] font-bold;
