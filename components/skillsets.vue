86f30802c22f3372d093830d274c140d3976936b
Kianmhz <kianmehr1777@gmail.com>
2023-11-08 14:15:04 -0500

    Add hover effect and style changes to skill icons

--- components/skillsets.vue
+++ components/skillsets.vue
@@ -4,8 +4,25 @@ const props = defineProps(['icon', 'title', 'content']);
 
 <template>
   <div>
-    <font-awesome-icon :icon="icon" size="2xl"/>
+    <font-awesome-icon class="skill-icon" :icon="icon" size="2xl"/>
     <h2>{{ title }}</h2>
     <p>{{ content }}</p>
   </div>
 </template>
+
+<style scoped>
+.skill-icon {
+  font-size: 40px;
+  transition: scale 0.2s ease-in-out;
+}
+
+.skill-icon:hover {
+  scale: 1.3;
+}
+
+h2 {
+  font-size: 1.5rem;
+  font-weight: 700;
+  margin: 10px 0;
+}
+</style>
