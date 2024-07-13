1d96d7c206319e56fd2c35745fdd24b9559da845
Kianmehr Haddad <haddad.kianm@gmail.com>
2024-07-12 23:07:09 -0400

    switched to button component for the contacts section

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,6 +1,4 @@
 <script setup>
-import { Icon } from '@iconify/vue'; // Assuming you're using Iconify for icons
-
 const contacts = ref([
   { title: "Linkedin", link: "https://www.linkedin.com/in/kianmhz/", description: "Connect with me professionally and let's build bridges.", icon: 'jam:linkedin-square' },
   { title: "Email", link: "mailto:haddad.kianm@gmail.com", description: "Reach out directly for inquiries, collaborations, or just to say hi.", icon: 'material-symbols:mail-outline' },
@@ -14,18 +12,11 @@ const contacts = ref([
       <div v-for="contact in contacts " class="contact-item">
         <h1 class="text-[--main-color] text-lg font-bold">{{ contact.title }}</h1>
         <p class="text-3xl leading-normal">{{ contact.description }}</p>
-        <button>
-          <NuxtLink
-            class="contact-button"
-            :to="contact.link"
-            target="_blank"
-          >
-            <div class="button-icon">
-              <Icon :icon="contact.icon" class="text-2xl" />
-            </div>
-            <div class="button-text">{{ contact.title }}</div>
-          </NuxtLink>
-        </button>
+        <BaseButton
+          :icon= contact.icon
+          :title= contact.title
+          :link= contact.link
+        />
       </div>
     </div>
   </UContainer>
