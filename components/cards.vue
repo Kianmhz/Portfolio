ee72411513aadd54747f95106e89329e582bbd62
Kianmehr Haddad <haddad.kianm@gmail.com>
2024-07-14 13:32:20 -0400

    Updated card title UI

--- components/cards.vue
+++ components/cards.vue
@@ -1,7 +1,7 @@
 <script setup>
 const cards = ref([
-  { id: 3, title: "Python Certification", src: "/python.jpg", content: "Certified in Python, I possess a strong foundation in scripting and backend development, showcasing my dedication to continuous learning in this versatile language."},
-  { id: 2, title: "C++ Certification", src: "/CPP.jpg", content: "A reflection to my proficiency in C++, this certification validates my skills in object-oriented programming and data structures." },
+  { id: 3, title: "Python Certification", src: "/img/python.jpg", content: "Certified in Python, I possess a strong foundation in scripting and backend development, showcasing my dedication to continuous learning in this versatile language."},
+  { id: 2, title: "C++ Certification", src: "/img/CPP.jpg", content: "A reflection to my proficiency in C++, this certification validates my skills in object-oriented programming and data structures." },
   { id: 1, title: "Professional Resume", src: "/ENGresume.pdf", content: "A blend of foundational knowledge and hands-on projects, showcasing my dedication to software engineering." }
 ]);
 </script>
@@ -19,9 +19,9 @@ const cards = ref([
         </div>
       </div>
       <div class="flex relative w-[75%] h-[350px] lg:h-[500px]">
-        <div v-for="card in cards" :key="card.id" class="resume-card" :id="`card${card.id}`">
+        <div v-for="card in cards" :key="card.id" class="resume-card" :id="'card' + card.id">
           <p class="text-3xl">{{ card.content }}</p>
-          <NuxtLink :href="card.src" target="_blank" rel="noopener noreferrer" class="absolute bottom-16 text-[--secondary-text-color] text-3xl">{{ card.title }}</NuxtLink>
+          <NuxtLink :to="card.src" external target="_blank" class="title">{{ card.title }}</NuxtLink>
         </div>
       </div>
     </div>
@@ -35,33 +35,50 @@ const cards = ref([
 }
 
 /* Card-specific styles for positioning and hover effect */
-.resume-card[id='card1'] {
+.title {
+  @apply absolute animate-pulse bottom-16 text-[--secondary-text-color] text-3xl transition-colors duration-300 ease-in-out;
+}
+
+/* Use more specific selectors for hover effects */
+.resume-card#card1 .title:hover {
+  color: var(--main-color);
+}
+
+.resume-card#card2 .title:hover {
+  color: var(--secondary-color);
+}
+
+.resume-card#card3 .title:hover {
+  color: var(--quaternary-color);
+}
+
+.resume-card#card1 {
   @apply z-[3] translate-x-[15%];
   border-left: 2px solid var(--main-color);
 }
 
-.resume-card[id='card2'] {
+.resume-card#card2 {
   @apply z-[2] brightness-[0.5];
   border-left: 2px solid var(--secondary-color);
 }
 
-.resume-card[id='card3'] {
+.resume-card#card3 {
   @apply z-[1] translate-x-[-15%] brightness-[0.5];
   border-left: 2px solid var(--quaternary-color);
 }
 
 /* Hover effects */
-.resume-card[id='card2']:hover,
-.resume-card[id='card3']:hover {
+.resume-card#card2:hover,
+.resume-card#card3:hover {
   @apply filter brightness-[1];
 }
 
-.resume-card[id='card3']:hover~.resume-card[id='card1'],
-.resume-card[id='card2']:hover~.resume-card[id='card1'] {
+.resume-card#card3:hover~.resume-card#card1,
+.resume-card#card2:hover~.resume-card#card1 {
   @apply transform translate-x-[100%] filter brightness-[0.5];
 }
 
-.resume-card[id='card3']:hover~.resume-card[id='card2'] {
+.resume-card#card3:hover~.resume-card#card2 {
   @apply transform translate-x-[85%] filter brightness-[0.5];
 }
 
